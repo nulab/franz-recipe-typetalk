d3caa5a5ff5344482179b69f07c90c3117747cdc
Makoto Motohashi <mkt.motohashi@gmail.com>
2021-01-15 14:50:55 +0900

    Remove dollar sign in favor of getElementsByClassName

--- webview.js
+++ webview.js
@@ -6,11 +6,15 @@ module.exports = (Franz) => {
   const likesActivitiesIndex = 3;
 
   const hasUnreads = (sidebarItems, index) => {
-    return Number($(sidebarItems.get(index)).hasClass(className));
+    if (!sidebarItems || sidebarItems.length < 1) {
+      return 0;
+    }
+    const sidebarItem = sidebarItems[index];
+    return Number(sidebarItem.classList.contains(className));
   };
 
   const getUnreads = () => {
-    const sidebarItems = $(".sidebar__item");
+    const sidebarItems = document.getElementsByClassName(".sidebar__item");
     const indirectMessages = hasUnreads(sidebarItems, topicsIndex) + hasUnreads(sidebarItems, likesActivitiesIndex);
     const directMessages = hasUnreads(sidebarItems, dmIndex) + hasUnreads(sidebarItems, notificationsIndex);
 
